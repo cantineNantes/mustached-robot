082a0c5fb0398d0c9aeb9f5ed719ae6feee5557a
Dzey <jeremie.pottier@gmail.com>
2012-07-12 23:56:21 +0200

    bugfix

---
+++ application/modules/coworking/controllers/calendar.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Calendar extends Public_Controller {
+
+	public function index()
+	{
+		$params = array('https://www.google.com/calendar/feeds/7m7vdg8v8k5q7u5hvrsnubvpco%40group.calendar.google.com/public/basic');
+		$this->load->library('coreylib');
+		$this->coreylib->
+	}
+
+}

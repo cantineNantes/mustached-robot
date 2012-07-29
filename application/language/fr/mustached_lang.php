3728cb6b247f322d2661545ca4a8fa3148647f84
Dzey <jeremie.pottier@gmail.com>
2012-07-29 15:36:42 +0200

    remove Codeigiter files

--- application/language/fr/mustached_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lang["welcome"]		    = "Bienvenue !";
-
-
-/* End of file mustached_lang.php */
-/* Location: ./application/language/fr/mustached_lang.php */
--- assets/js/common.coffee
+++
@@ -1,3 +0,0 @@
-$(document).ready ->	
-	$("abbr.timeago").timeago()
-	a = $('#companies').autocomplete({ serviceUrl:'/user/companies' })

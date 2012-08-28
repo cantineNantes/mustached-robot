9d9e869ab45cd00b47c965228b972d13b349e23e
Dzey <jeremie.pottier@gmail.com>
2012-08-28 23:32:51 +0200

    bugfix

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -3,12 +3,25 @@
  * The development database settings.
  */
 
+/*
 return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=mustached',
 			'username'   => 'root',
 			'password'   => 'root',
+		),
+	),
+);
+
+ */
+/* PHPFOG Configuration */
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host='.getenv('MYSQL_DB_HOST').';dbname='.getenv('MYSQL_DB_NAME'),
+			'username'   => getenv('MYSQL_USERNAME'),
+			'password'   => getenv('MYSQL_PASSWORD'),
 		),
 	),
 );

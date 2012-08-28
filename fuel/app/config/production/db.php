271990226b17c9805e020396d125687face718a1
Dzey <jeremie.pottier@gmail.com>
2012-08-28 23:20:01 +0200

    conf for phpfog

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -1,14 +1,42 @@
 <?php
+
 /**
  * The production database settings.
  */
 
+
+// Get the database data from environment variable. See http://support.cloudfoundry.com/entries/20386327-php-support-in-cloud-foundry
+
+/* APP FOG CONFIGURATION */
+
+/*
+$services = getenv("VCAP_SERVICES");
+$services_json = json_decode($services,true);
+
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+
+print_r($mysql_config);
+
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host='.$mysql_config["hostname"].';dbname='.$mysql_config["name"],
+			'username'   => $mysql_config["user"],
+			'password'   => $mysql_config["password"],
 		),
 	),
 );
+
+ */
+ 
+/* PHPFOG Configuration */
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host='.getenv('MYSQL_DB_HOST').';dbname='.getenv('MYSQL_DB_NAME'),
+			'username'   => getenv('MYSQL_USERNAME'),
+			'password'   => getenv('MYSQL_PASSWORD'),
+		),
+	),
+);
+
---
+++ fuel/app/modules/calendar/tests/manager.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace Calendar;
+
+/**
+ * Manager class tests
+ *
+ * @group App
+ * @group Calendar
+ */
+class Test_Manager extends \TestCase
+{
+
+	private $calendarMock;
+	private $manager;
+
+	public function setUp()
+	{
+		\Module::load('calendar');
+
+		$this->calendarMock = $this->getMock('Gcalendar', array('getEvents', 'authenticate'));
+
+		$this->calendarMock->expects($this->any())
+                           ->method('authenticate')
+                           ->will($this->returnValue(true));
+
+        $this->calendarMock->expects($this->any())
+                           ->method('getEvents')
+                           ->will($this->returnCallback(array($this, 'returnEvents')));
+
+        $this->manager = new Manager;
+		$this->manager->setCalendar($this->calendarMock);
+
+	}
+
+	public function test_get_next_event_has_20_events_by_default()
+	{
+		
+		$e = $this->manager->get_next_events();
+		$this->assertSame(20, sizeof($e));
+	}
+
+	public function test_get_next_5_events_return_5_events()
+	{
+		$e = $this->manager->get_next_events(5);
+		$this->assertSame(5, sizeof($e));
+	}
+
+
+	private function createEvents($number)
+	{
+		$events = new \stdClass;
+		$events->data->items = array();
+		for($i = 1; $i<($number+1); $i++)
+		{
+			$events->data->items[] = array('title' => 'Event number '.$i);
+		}
+		return $events;
+	}
+
+	public function returnEvents()
+	{
+		$args = func_get_args();
+		return $this->createEvents($args[1]);
+	}
+
+}

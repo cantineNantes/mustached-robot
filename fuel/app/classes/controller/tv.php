cb08b6bb3e2f2b6c2409f9aabde0f2a0170a711a
Dzey <jeremie.pottier@gmail.com>
2012-09-10 21:51:22 +0200

    oops, just forgot a ";"

--- fuel/app/classes/controller/tv.php
+++ fuel/app/classes/controller/tv.php
@@ -8,7 +8,7 @@ class Controller_Tv extends \Controller_Base
 
 	public function action_index()
 	{
-		$this->data['screens'] = array('', '')
+		$this->data['screens'] = array('', '');
 		return $this->_render('tv');
 	}
 

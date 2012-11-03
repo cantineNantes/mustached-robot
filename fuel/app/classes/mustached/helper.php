3eb99958d29b1489ff0f82e1ed33a4432b2b80a6
Dzey <jeremie.pottier@gmail.com>
2012-11-03 14:28:24 +0100

    change default gravatar

--- fuel/app/classes/mustached/helper.php
+++ fuel/app/classes/mustached/helper.php
@@ -6,7 +6,7 @@ class Helper {
 
 	public static function avatar($email, $size = '80')
 	{
-		return 'http://www.gravatar.com/avatar/'.md5($email).'?s='.$size;
+		return 'http://www.gravatar.com/avatar/'.md5($email).'?s='.$size.'&d=mm';
 	}
 
 }
